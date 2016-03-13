63cfa5f94bfbe38daf97c08eedf9350c8f8ee4b5
KaFai C <kafaicoder@gmail.com>
2016-03-13 14:45:10 +0800

    INITIAL COMMIT

---
+++ js/index.js
@@ -0,0 +1,33 @@
+window.onload = function() {
+  var SUCCESS_MESSAGE_SHOW_TIME = 1500;
+  var myDataRef = new Firebase('https://vegsochk.firebaseio.com/subscribers');
+  var form = document.getElementById('subscribeForm');
+  var emailInputField = document.getElementById('emailInput')
+  var loadingCover = document.getElementById('loading');
+  form.addEventListener("submit", processForm);
+
+  function getEmailInputValue() {
+    return emailInputField.value;
+  }
+
+  function processForm(e) {
+    e.preventDefault();
+    saveEmail(getEmailInputValue());
+  }
+
+  function saveEmail(email) {
+    myDataRef.push({email: email}, function() {
+      displayLoadingCover();
+      setTimeout(hideLoadingCover, SUCCESS_MESSAGE_SHOW_TIME);
+    });
+  }
+
+  function displayLoadingCover() {
+    loadingCover.className = 'show';
+  }
+
+  function hideLoadingCover() {
+    loadingCover.className = '';
+  }
+
+}
